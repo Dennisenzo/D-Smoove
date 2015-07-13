8491e19d8114c0253a0eb2e39daf68947bf03200
Dennis Hellewegen <d.hellewegen@gmail.com>
2015-07-13 19:14:52 +0200

    refactoring

---
+++ QueueTest/WorkQueue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTest
+{
+    public class WorkQueue<T>
+    {
+        private ConcurrentQueue<T> _innerQueue;
+
+
+
+        public WorkQueue()
+        {
+            _innerQueue = new ConcurrentQueue<T>();
+        }
+
+        public void Enqueue()
+        {
+
+        }
+    }
+}
